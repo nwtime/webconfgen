2dfae055bd299dee60edaaf764e0e46236f6c8c0
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-07-06 03:56:12 +0300

    sucessfully post an edit and redirect to page added defers to code for better loading

--- frontend/static/frontend/js/snippets/Snippets.js
+++ frontend/static/frontend/js/snippets/Snippets.js
@@ -1,4 +1,9 @@
 (function() {
     'use strict';
-    angular.module('snippets', ['ngMaterial']);
+    angular
+        .module('snippets', ['ngMaterial'])
+            .config(function($httpProvider) {
+                $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+                $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+            });
 })();
