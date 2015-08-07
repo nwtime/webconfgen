f4de70b095b5ac12da0fc5d068557bf03532dbff
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-08-07 14:46:20 +0530

    renamed media variables to make it easier to work with

--- webconfgen/urls.py
+++ webconfgen/urls.py
@@ -20,4 +20,4 @@ urlpatterns = patterns(
 
     url(r'^$', views.type_generate, name="type-generate-detail"),
     url(r'^[0-9a-fA-F]{8}-?[0-9a-fA-F]{4}-?4[0-9a-fA-F]{3}-?[89abAB][0-9a-fA-F]{3}-?[0-9a-fA-F]{12}/', views.type_parse, name="type-parse-detail"),
-) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
