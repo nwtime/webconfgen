2896589077fbf97f1e84c1a1e3913ab96e5e1f74
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-08-12 20:25:27 +0530

    Switched to tabbed views for displaying outputs.

--- frontend/static/frontend/js/uploads/UploadsController.js
+++ frontend/static/frontend/js/uploads/UploadsController.js
@@ -3,20 +3,64 @@
     angular
         .module('uploads')
             .controller('UploadsController', [
-                'UploadsService', '$log', '$q',
+                'UploadsService', '$log', '$q', '$timeout',
                 UploadsController
         ]);
-    function UploadsController(UploadsService, $log, $q) {
+    function UploadsController(UploadsService, $log, $q, $timeout) {
         var self = this;
-        self.inputTimeout = null;
-        self.outputTimeout = null;
+        self.inputUrl = null;
+        self.outputUrl = null;
+        self.inputLoading = true;
+        self.outputLoading = true;
+        self.selectedTab = 1;
+        self.fileRaw = fileRaw;
+        self.fileUpload = fileUpload;
         self.uuid = window.location.toString().split('/')[3];
-        UploadsService
-            .getUpload(self.uuid)
-                .then(function(response) {
-                    self.upload = response.data;
-                    $log.debug(self.upload);
-                });
+
+        function refreshUpload() {
+            UploadsService
+                .getUpload(self.uuid)
+                    .then(function(response) {
+                        self.upload = response.data;
+                        refreshInputFile();
+                        refreshOutputFile();
+                    });
+        }
+
+        function refreshInputFile() {
+            if (self.upload.input_file_url) {
+                if (self.upload.input_file_url != self.inputUrl) {
+                    self.inputLoading = true;
+                    self.inputUrl = self.upload.input_file_url;
+                    UploadsService
+                        .getInputFile(self.upload)
+                            .then(function(response) {
+                                self.editorInput.setValue(response.data, -1);
+                                self.inputLoading = false;
+                            });
+                }
+            } else {
+                $timeout(refreshUpload, 1000);
+            }
+        }
+
+        function refreshOutputFile() {
+            if (self.upload.output_file_url) {
+                if (self.upload.output_file_url != self.outputUrl) {
+                    self.outputLoading = true;
+                    self.outputUrl = self.upload.output_file_url;
+                    UploadsService
+                        .getOutputFile(self.upload)
+                            .then(function(response) {
+                                self.editorOutput.setValue(response.data, -1);
+                                self.outputLoading = false;
+                            });
+                }
+            } else {
+                $timeout(refreshUpload, 1000);
+            }
+        }
+
         function loadEditor() {
             self.editorInput = ace.edit('editor_input');
             self.editorInput.setTheme('ace/theme/chrome');
@@ -28,6 +72,22 @@
             self.editorOutput
                 .getSession()
                     .setMode('ace/mode/plain_text');
+            refreshUpload();
+        }
+
+        function fileRaw() {
+            var code = self.editorOutput.getValue();
+            var blob = new Blob([code], {type: 'text/plain'});
+            var url = URL.createObjectURL(blob);
+            window.open(url, '_blank');
+        }
+
+        function fileUpload() {
+            self.inputLoading = true;
+            var content = self.editorInput.getValue();
+            UploadsService
+                .updateContent(content, self.upload)
+                    .then(function(response) {});
         }
         angular.element(document).ready(loadEditor);
     };
--- frontend/static/frontend/js/uploads/UploadsService.js
+++ frontend/static/frontend/js/uploads/UploadsService.js
@@ -9,10 +9,25 @@
     function UploadsService($http) {
         return {
             getUpload: function(uuid) {
-                return $http.get('/api/uploads/' + uuid + '/mini/');
+                return $http.get('/api/uploads/' + uuid + '/mini/', {
+                    cache: false
+                });
             },
-            getFile: function(url) {
-                return $http.get('');
+            getInputFile: function(upload) {
+                return $http.get(upload.input_file_url, {
+                    cache: true
+                });
+            },
+            getOutputFile: function(upload) {
+                return $http.get(upload.output_file_url, {
+                    cache: true
+                });
+            },
+            updateContent: function(content, upload) {
+                return $http.put(upload.url, {
+                    'version': upload.version,
+                    'input_string': content
+                });
             }
         };
     };
