ba84f9165ba14c80d478ac6dc469526c1aa31272
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2015-08-19 05:11:32 +0530

    Added scaffolding for backend parser

--- frontend/permissions.py
+++ frontend/permissions.py
@@ -3,16 +3,16 @@ from rest_framework import permissions
 
 class IsOwnerOrAnonOrReadOnly(permissions.BasePermission):
     """
-        The object must have an attribute uploads_owner which may be null.
+        The upload must have an attribute uploads_owner which may be null.
         Edit permissions are given only when request.user is owner
         or uploads_owner is null
     """
 
-    def has_object_permission(self, request, view, object):
+    def has_object_permission(self, request, view, upload):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        if object.uploads_owner is None:
+        if upload.uploads_owner is None:
             return True
 
-        return object.uploads_owner == request.user
+        return upload.uploads_owner == request.user
